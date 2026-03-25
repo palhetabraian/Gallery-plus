9085ef2531e23b497bc677f8db3ea683ecdedf0b
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-03-24 21:50:31 -0300

    Montando layout do componente InputSingleFile - Estado com arquivo

--- src/components/input-single-file.tsx
+++ src/components/input-single-file.tsx
@@ -1,7 +1,8 @@
 import { type VariantProps, tv } from 'tailwind-variants';
 import Icon from './icon';
-import Text from './text';
+import Text, { textVariants } from './text';
 import UploadFileIcon from '../assets/icons/upload-file.svg?react';
+import FileImageIcon from '../assets/icons/image.svg?react';
 
 export const inputSingleFileVariants = tv({
     base: `
@@ -32,9 +33,12 @@ export const inputSingleFileIconVariants = tv({
     },
 });
 
-interface InputsingleFileProps extends VariantProps<typeof inputSingleFileVariants>, React.ComponentProps<'input'> {}
+interface InputsingleFileProps
+    extends VariantProps<typeof inputSingleFileVariants>, Omit<React.ComponentProps<'input'>, 'size'> {
+    error?: React.ReactNode;
+}
 
-export default function InputSingleFile({ size }: InputsingleFileProps) {
+export default function InputSingleFile({ size, error }: InputsingleFileProps) {
     return (
         <div>
             <div className="w-full relative group cursor-pointer">
@@ -51,6 +55,39 @@ export default function InputSingleFile({ size }: InputsingleFileProps) {
                         Arraste o arquivo aqui <br />
                         ou clique para selecionar
                     </Text>
+                </div>
+            </div>
+            {error && (
+                <Text variant="label-small" className="text-accent-red">
+                    Erro no campo
+                </Text>
+            )}
+
+            <div
+                className={`
+                flex gap-3 items-center 
+                border border-solid border-border-primary mt-5
+                p-3 rounded
+                `}
+            >
+                <Icon svg={FileImageIcon} className="fill-white w-6 h-6" />
+                <div className="flex flex-col">
+                    <div className="truncate max-w-80 ">
+                        <Text variant="label-medium" className="text-placeholder">
+                            Nome do arquivo.png
+                        </Text>
+                    </div>
+                    <div className="flex">
+                        <button
+                            type="button"
+                            className={textVariants({
+                                variant: 'label-small',
+                                className: 'text-accent-red cursor-pointer hover:underline',
+                            })}
+                        >
+                            Remover
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
