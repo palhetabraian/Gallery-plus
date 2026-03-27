47782e17b678723a5bf8933f3c42353ac0f012bf
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-03-26 22:31:03 -0300

    Estruturando roteamento

---
+++ src/pages/layout-main.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router';
+import Text from '../components/text';
+
+export default function LayoutMain() {
+    return (
+        <>
+            <Text variant="heading-large">Layout main</Text>
+            <Outlet />
+        </>
+    );
+}
