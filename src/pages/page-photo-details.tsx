b07b7199ec77a7e09882bccb728e495025693ce7
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-03-31 09:22:54 -0300

    Mock da pagina de detalhes da foto - Imagem e bot

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -4,11 +4,13 @@ import Container from '../components/container';
 import type { Photo } from '../contexts/photos/models/photo';
 import Skeleton from '../components/skeleton';
 import PhotosNavigator from '../contexts/photos/components/photos-navigator';
+import ImagePreview from '../components/image-preview';
+import Button from '../components/button';
 
 export default function PagePhotoDetails() {
     const { id } = useParams();
     // apenas para fazer o teste do mock
-    const isLoadingPhoto = false;
+    const isLoadingPhoto = true;
     const photo = {
         id: '123',
         title: 'Ola mundo',
@@ -31,6 +33,25 @@ export default function PagePhotoDetails() {
 
                 <PhotosNavigator loading={isLoadingPhoto} />
             </header>
+
+            <div className="grid grid-cols-[21rem] gap-24">
+                <div className="space-y-3">
+                    {!isLoadingPhoto ? (
+                        <ImagePreview
+                            src={`/images/${photo?.imageId}`}
+                            title={photo?.title}
+                            imageClassName="h-[21rem]"
+                        />
+                    ) : (
+                        <Skeleton className="h-[21rem]" />
+                    )}
+                    {!isLoadingPhoto ? (
+                        <Button variant="destructive">Excluir</Button>
+                    ) : (
+                        <Skeleton className="w-20 h-10" />
+                    )}
+                </div>
+            </div>
         </Container>
     );
 }
