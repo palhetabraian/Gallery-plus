50d16165fc7e30cbcf1658d75d4c3ba24ecd16ef
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-03-30 22:45:11 -0300

    Componente PhotoWidget

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,9 +1,25 @@
-import Text from '../components/text';
+import Container from '../components/container';
+import PhotoWidget from '../contexts/photos/components/photo-widget';
+import type { Photo } from '../contexts/photos/models/photo';
 
 export default function PageHome() {
     return (
-        <>
-            <Text variant="heading-medium">Pagina Oficial</Text>
-        </>
+        <Container>
+            <div className="grid grid-cols-4 gap-9">
+                <PhotoWidget
+                    photo={{
+                        id: '123',
+                        title: 'Ola mundo',
+                        imageId: 'portrait-tower.png',
+                        albums: [
+                            { id: '321', title: 'album 1' },
+                            { id: '123', title: 'album 2' },
+                            { id: '456', title: 'album 3' },
+                        ],
+                    }}
+                />
+                <PhotoWidget photo={{} as Photo} loading />
+            </div>
+        </Container>
     );
 }
