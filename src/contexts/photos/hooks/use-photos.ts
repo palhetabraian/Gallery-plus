bae65afebcd2b38817157add2b56c5ba4f8e6925
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-04-07 21:44:26 -0300

    Adicionando filtro por álbum na lista de fotos

--- src/contexts/photos/hooks/use-photos.ts
+++ src/contexts/photos/hooks/use-photos.ts
@@ -1,15 +1,26 @@
 import { useQuery } from '@tanstack/react-query';
 import type { Photo } from '../models/photo';
 import { fetcher } from '../../../helpers/api';
+import { useQueryState, createSerializer, parseAsString } from 'nuqs';
+
+const toSearchParams = createSerializer({
+    albumId: parseAsString,
+});
 
 export default function usePhotos() {
+    const [albumId, setAlbumId] = useQueryState('albumId');
+
     const { data, isLoading } = useQuery<Photo[]>({
-        queryKey: ['photos'],
-        queryFn: () => fetcher('/photos'),
+        queryKey: ['photos', albumId],
+        queryFn: () => fetcher(`/photos${toSearchParams({ albumId })}`),
     });
 
     return {
         photos: data || [],
         isLoadingPhotos: isLoading,
+        filters: {
+            albumId,
+            setAlbumId,
+        },
     };
 }
